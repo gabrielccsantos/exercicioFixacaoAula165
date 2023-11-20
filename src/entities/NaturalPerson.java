eaf8c1f143405dd8e63d57661c166c50087f3f79
Gabriel Santos <gabrielcarneirocsantos@gmail.com>
2023-11-20 16:55:01 -0300

    Classe NaturalPerson

---
+++ src/entities/NaturalPerson.java
@@ -0,0 +1,32 @@
+package entities;
+
+public abstract class NaturalPerson extends Person{
+    private Double spentOnHealth;
+
+    public NaturalPerson(String name, Double anualIncome, Double spentOnHealth) {
+        super(name, anualIncome);
+        this.spentOnHealth = spentOnHealth;
+    }
+
+    public Double getSpentOnHealth() {
+        return spentOnHealth;
+    }
+
+    public void setSpentOnHealth(Double spentOnHealth) {
+        this.spentOnHealth = spentOnHealth;
+    }
+
+    @Override
+    public Double calculateTax(){
+        if(getAnualIncome() < 20.000){
+            return (getAnualIncome() * 0.15) - (getSpentOnHealth() * 0.5);
+        }else if(getAnualIncome() >= 20.000){
+            return (getAnualIncome() * 0.25) - (getSpentOnHealth() * 0.5);
+        }
+        return 0.0;
+    }
+
+    public String toString(){
+        return getName() + " - $" + String.format("%.2f", calculateTax());
+    }
+}
